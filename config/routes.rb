0553f2645fca107a8e5752211e013a11c09c9777
Nicholas Major <nick@nmajor.com>
2018-06-07 19:54:32 +0100

    Added tests and travis config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index' # And remove the generated route `products/index`
+
+  resource :products, only: [ :new, :create ]
 end
